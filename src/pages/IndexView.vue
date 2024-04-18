327c1ac9fb0641dd4b2835d22ddf8ab0750296bb
nosqd <nosqdgg@gmail.com>
2024-04-19 00:21:10 +0300

    @shadcn/vue

--- src/pages/IndexView.vue
+++ src/pages/IndexView.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import {useCounterStore} from "../stores/counter"
+import Button from "@/components/ui/button/Button.vue";
+
 const counter = useCounterStore();
 </script>
 
 <template>
   <main>
     <h1>Counter: <span> {{ counter.count }} </span></h1>
-    <button @click="counter.increment()">Increment</button>
-    <button @click="counter.decrement()">Decrement</button>
+    <Button @click="counter.increment()">Increment</Button>
+    <Button @click="counter.decrement()">Decrement</Button>
   </main>
 </template>
