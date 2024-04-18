1561cf8e11db5d30111b35614288947202f104f0
nosqd <nosqdgg@gmail.com>
2024-04-19 00:13:23 +0300

    initial commit

---
+++ src/views/IndexView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import {useCounterStore} from "../stores/counter"
+const counter = useCounterStore();
+</script>
+
+<template>
+  <main>
+    <h1>Counter: <span> {{ counter.count }} </span></h1>
+    <button @click="counter.increment()">Increment</button>
+    <button @click="counter.decrement()">Decrement</button>
+  </main>
+</template>
